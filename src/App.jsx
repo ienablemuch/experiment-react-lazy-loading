c0a6638f5aafbe7e2bd357ed394ec81d131b678a
dev <me@ienablemuch.com>
2019-08-08 19:58:00 +0800

    used yarn plugnplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React, { Suspense, lazy } from 'react';
+import React, { Suspense, lazy } from "react";
 
 import ErrorBoundary from "./ErrorBoundary";
 
-const OtherComponent = lazy(() => import('./OtherComponent'));
-const AnotherComponent = lazy(() => import('./AnotherComponent'));
+const OtherComponent = lazy(() => import("./OtherComponent"));
+const AnotherComponent = lazy(() => import("./AnotherComponent"));
 
 const App = () => (
     <div>
         <ErrorBoundary>
             <Suspense fallback={<div>Loading...</div>}>
                 <section>
-                    <OtherComponent/>                    
-                    <AnotherComponent/>
+                    <OtherComponent />
+                    <AnotherComponent />
                 </section>
             </Suspense>
         </ErrorBoundary>
-    </div>  
+    </div>
 );
 
 export default App;
